d1e34723bc5708ee5451c23218ce5568c51a39b3
Konstantin Welke <konstantin.welke@citrix.com>
2016-04-08 12:16:27 +0200

    Improve startup message

--- src/main/java/com/citrix/playground/test/kosta/App.java
+++ src/main/java/com/citrix/playground/test/kosta/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println(Void.class.getSimpleName());
+        System.out.println("Void: " + Void.class.getSimpleName());
     }
 }
